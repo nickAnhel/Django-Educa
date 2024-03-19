6a5d5f82828aacd4724a1928e2db151491a0345c
nickAnhel <anhimovn1@gmail.com>
2024-03-19 20:17:38 +0300

    Ch.12 Added static media settings

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media settings
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
