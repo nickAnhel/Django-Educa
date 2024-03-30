4234bb9c68ec802ee41f1fb23026b83818aebee1
nickAnhel <anhimovn1@gmail.com>
2024-03-30 14:44:54 +0300

    Ch.14 Added views and templates for course detail and list pages

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,16 +1,17 @@
-import json
 from django.apps import apps
+from django.db.models import Count
 from django.forms import modelform_factory
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.base import View, TemplateResponseMixin
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
-from .models import Course, Module, Content, ItemBase
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
 
@@ -150,3 +151,23 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for content_id, order in self.request_json.items():
             Content.objects.filter(id=content_id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({"saved": "OK"})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request: HttpRequest, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response({"subjects": subjects, "subject": subject, "courses": courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
