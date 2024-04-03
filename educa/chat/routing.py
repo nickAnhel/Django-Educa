89aef3fdc039c79b60fc92378523c08faf8de55f
nickAnhel <anhimovn1@gmail.com>
2024-04-03 20:56:19 +0300

    Ch.16 Added echo websocket server

--- educa/chat/routing.py
+++ educa/chat/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/room/($P<course_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/room/(?P<course_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
 ]
